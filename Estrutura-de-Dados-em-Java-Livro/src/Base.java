6fbba032ab50537e6093b5c21c6bba0480d4a25d
Icaro Gabriel <icarogabrielpc05@gmail.com>
2025-04-07 16:55:34 -0300

    modificacao das pastas iniciais

---
+++ Estrutura-de-Dados-em-Java-Livro/src/Base.java
@@ -0,0 +1,20 @@
+public class Base {
+    public static void main(String[] args) {
+        boolean flag = true;
+        char ch = 'A';
+        byte b = 12;
+        short s = 24;
+        int i = 257;
+        long l = 890L; // Observar o uso do "L" aqui 
+        float f = 3.1415F; // Observar o uso do "F" aqui
+        double d = 2.1828;
+        System.out.println("flag = " + flag); // o "+" indica concatenacao de strings
+        System.out.println("ch = " + ch);
+        System.out.println("b = " + b);
+        System.out.println("s = " + s);
+        System.out.println("i = " + i);
+        System.out.println("l = " + l);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+    }
+}
